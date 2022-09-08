3fa18fe58f481b22b9b7767fdf941ffc4db13120
Alexander Pataridze <s.pataridze@gmail.com>
2022-09-08 18:44:24 +0400

    feat: isHeatMapViewInstalled func

--- src/index.js
+++ src/index.js
@@ -2,15 +2,43 @@ import Config from "./config";
 import heatMapColorForValue from "./heatMapColorForValue";
 
 const ViewNativeComponent = require("react-native/Libraries/Components/View/ViewNativeComponent");
+const View = ViewNativeComponent.default;
+
+let initialized = false;
 let installed = false;
 
 function initHeatMapView(config) {
-  if (!installed) {
+  if (!initialized) {
     Object.assign(Config, config);
-    const HeatView = require("./heatView").default;
-    ViewNativeComponent.default = HeatView;
+    installHeatMapView();
+    initialized = true;
   }
-  installed = true;
 }
 
-export { Config, heatMapColorForValue, initHeatMapView };
+function installHeatMapView() {
+  if (!installed && Config.enabled) {
+    const HeatView = require("./heatView").default;
+    ViewNativeComponent.default = HeatView;
+    installed = true;
+  }
+}
+
+function uninstallHeatMapView() {
+  if (installed) {
+    ViewNativeComponent.default = View;
+    installed = false;
+  }
+}
+
+function isHeatMapViewInstalled() {
+  return installed;
+}
+
+export {
+  heatMapColorForValue,
+  Config,
+  initHeatMapView,
+  installHeatMapView,
+  uninstallHeatMapView,
+  isHeatMapViewInstalled,
+};
