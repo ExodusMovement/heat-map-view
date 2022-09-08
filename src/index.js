4480c1983729be0fa75d379bdf15ac466855a393
Alexander Pataridze <s.pataridze@gmail.com>
2022-09-08 15:48:04 +0400

    feat: export config

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import Config from "./config";
 const ViewNativeComponent = require("react-native/Libraries/Components/View/ViewNativeComponent");
 let installed = false;
 
-export async function initHeatMapView(config) {
+function initHeatMapView(config) {
   if (!installed) {
-    Object.assign(Config, config)
+    Object.assign(Config, config);
     const HeatView = require("./heatView").default;
     ViewNativeComponent.default = HeatView;
   }
   installed = true;
 }
+
+export { Config, initHeatMapView };
